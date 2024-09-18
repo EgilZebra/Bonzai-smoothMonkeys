aa4ab1581ac7a5cc7eb9899dabe13dad9e6c42ba
pettersson932 <144891931+pettersson932@users.noreply.github.com>
2024-09-18 20:25:33 +0200

    x

--- functions/Employee/allReservations/index.js
+++ functions/Employee/allReservations/index.js
@@ -1,16 +1,38 @@
 import { responseMaker } from "../../services/responseMaker";
 import { db } from "../../../data";
 
-
 exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v4! Your function executed successfully!",
-    }),
+  const params = {
+    TableName: "Bookings",
+    ProjectionExpression:
+      "BookingId, startDate, endDate, guests, rooms, customer",
   };
+
+  try {
+    const book = await db.scan(params).promise();
+    const bookings = book.Items.map((item) => ({
+      BookingNumber: item.BookingId,
+      CheckInDate: item.startDate,
+      CheckOutDate: item.endDate,
+      NumberOfGuests: item.guests,
+      NumberOfRooms: item.rooms,
+      CustomerName: item.customer,
+    }));
+
+    return responseMaker(200, bookings);
+  } catch (error) {
+    return responseMaker(500, { message: "cant find bookings" });
+  }
 };
 
+//exports.handler = async (event) => {
+//  return {
+//    statusCode: 200,
+//    body: JSON.stringify({
+//      message: "Go Serverless v4! Your function executed successfully!",
+//    }),
+//  };
+//};
 
 // exports.handler = async (event) => {
 //   // const bookings = {
